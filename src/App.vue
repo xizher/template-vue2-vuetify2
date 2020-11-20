660425f11a283213da83a68e7ce1e15a48d937fc
xizher <xizher@163.com>
2020-11-20 19:41:50 +0800

    feat: *

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-btn large>Normal</v-btn>
+    <v-btn color="primary">Primary</v-btn>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { onMounted } from '@vue/composition-api'
+export default {
+  name: '',
+  setup () {
+    onMounted(() => {
+      console.log(new Date().format('yyyy/MM/dd'))
+    })
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
